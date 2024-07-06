76d263ebf6128299da9d5e10d5e836dbc4ecded0
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-07-06 21:37:32 +0530

    creates Hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <main className='bgImage'>
+        <section>
+            <div className='container'>
+               {/*Navbar section */}
+               {/*Hero section */}
+                 <div className='grid grid-cols-1 md:grid-cols-2'>
+
+                 </div>
+            </div>
+        </section>
+    </main>
   )
 }
 
