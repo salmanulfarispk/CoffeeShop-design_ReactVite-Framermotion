b7bc9fb3696be2ae05d1e037eb9c5b6b7e40d68d
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-07-07 22:30:40 +0530

    sidebar completes..

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,52 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BgImage from "../../assets/bg-slate.png"
 import BlackCoffe from "../../assets/black.png"
 import { Navbar } from '../Navbar/Navbar'
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+
+
+
+const bgImage = {
+  backgroundImage: `url(${BgImage})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat"
+}
 
 const Hero = () => {
 
-  const bgImage = {
-    backgroundImage: `url(${BgImage})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat"
-  }
+  const [openSidebar, setSidebarOpen] = useState(false)
 
   return (
     <main style={bgImage}>
-      <section className='min-h-[750px] w-full'>
+      <section className='relative min-h-[750px] w-full'>
         <div className='container'>
 
           {/*Navbar section */}
-            <Navbar/>
+          <Navbar openSidebar={openSidebar} setSidebarOpen={setSidebarOpen} />
           {/*Hero section */}
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]'>
             {/*text content section */}
             <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
 
               <motion.h1 className='text-7xl font-bold leading-tight ml-14'
-                initial={{opacity: 0, y: -100}}
-                animate={{opacity: 1, y: 0}}
-                 transition={{
-                  type:"spring",
-                   stiffness: 100,
-                   damping: 10,
-                   delay: 1,
-                   }}
-                >
-                Blvck Tumbler
-                </motion.h1>
-              <motion.div className='relative'
-                initial={{opacity: 0, y: 100}}
-                animate={{opacity: 1, y: 0}}
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
                 transition={{
-                 type:"spring",
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 1,
+                }}
+              >
+                Blvck Tumbler
+              </motion.h1>
+              <motion.div className='relative'
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  type: "spring",
                   stiffness: 100,
                   damping: 10,
                   delay: 1.2,
-                  }}  
-               >
+                }}
+              >
                 <div className='relative z-10 space-y-4'>
                   <h1 className='text-2xl'>Black Lifestyle Lovers</h1>
                   <h1 className='text-sm opacity-55 leading-loose'> Every sip fuels ambition and connection turning everyday moments into cherished rituals. Embrace the
@@ -61,40 +69,40 @@ const Hero = () => {
             {/*Hero image section */}
             <div className='relative'>
               <motion.img src={BlackCoffe}
-                 initial={{opacity: 0, scale: 0}}
-                 animate={{opacity: 1, scale: 1}}
-                 transition={{
-                  type:"spring",
-                   stiffness: 100,   //it determines how fast the spring snaps back to its original position.Higher values results quicker animation and Lower values create a softer animation.
-                   damping: 10,      //it controls how much the spring oscillates or bounces before settling.Higher values results more damping, which slows down the animation.so lower values is better.
-                   delay: 0.4,
-                   }}  
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,   //it determines how fast the spring snaps back to its original position.Higher values results quicker animation and Lower values create a softer animation.
+                  damping: 10,      //it controls how much the spring oscillates or bounces before settling.Higher values results more damping, which slows down the animation.so lower values is better.
+                  delay: 0.4,
+                }}
                 alt='coffee-main'
                 className='relative z-40 h-[400px] md:h-[700px] img-shadow'
               />
               {/*orange Ring circle */}
               <motion.div className='absolute top-24 -right-16
                  border-primary rounded-full w-[180px] h-[180px] border-[20px] z-10'
-                 initial={{opacity: 0, y: 100}}
-                 animate={{opacity: 1, y: 0}}
-                 transition={{
-                  type:"spring",
-                   stiffness: 100,
-                   damping: 10,      
-                   delay: 0.8,
-                   }}  
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 0.8,
+                }}
               >
               </motion.div>
               {/*background big text */}
               <motion.div className='absolute -top-20 left-[200px] z-[1]'
-               initial={{opacity: 0, x: -100}}
-               animate={{opacity: 1, x: 0}}
-               transition={{
-                type:"spring",
-                 stiffness: 100,
-                 damping: 10,
-                 delay: 0.8,
-                 }}  
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 0.8,
+                }}
               >
                 <h1 className='text-[140px] scale-150 font-bold text-darkGray/40 leading-none '>
                   Blvck Tumbler
@@ -105,21 +113,21 @@ const Hero = () => {
 
             {/*Third div section */}
             <motion.div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'
-              initial={{opacity: 0, y: 100}}
-              animate={{opacity: 1, y: 0}}
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
               transition={{
-               type:"spring",
+                type: "spring",
                 stiffness: 100,
                 damping: 10,
                 delay: 1.2,
-                }}  
+              }}
             >
 
               <h1 className='opacity-0 text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
               <div className='relative'>
                 <div className='relative z-10 space-y-4'>
                   <h1 className='text-2xl'>Blvck Tumbler</h1>
-                  <h1 className='text-sm opacity-55 leading-loose'>Experience the pure essence of coffee with our black tumbler brew. Bold, robust, and unapologetically rich—perfect 
+                  <h1 className='text-sm opacity-55 leading-loose'>Experience the pure essence of coffee with our black tumbler brew. Bold, robust, and unapologetically rich—perfect
                     for those who savor the true taste of coffee.</h1>
                 </div>
                 <div className='absolute -top-6 -right-10 w-[275px] h-[190px] bg-darkGray/50 '>
@@ -130,6 +138,34 @@ const Hero = () => {
 
           </div>
         </div>
+
+        {/** sidebar menu section  */}
+
+        {openSidebar && (
+          <div className='absolute top-0 right-0 w-[140px] h-full
+           bg-gradient-to-b from-primary/80 to-primaryDArk/80 backdrop-blur-sm z-10'>
+            <div className='w-full h-full flex justify-center items-center'>
+              <div className='flex flex-col justify-center items-center gap-6 text-white'>
+                {/**line */}
+                <div className='w-[1px] h-[70px] bg-white'></div>
+
+                {/**social icons */}
+                <div className='inline-block rounded-full p-2 cursor-pointer border border-white'>
+                  <FaFacebookF className='text-2xl' />
+                </div>
+                <div className='inline-block rounded-full p-2 cursor-pointer border border-white'>
+                  <FaTwitter className='text-2xl' />
+                </div>
+                <div className='inline-block rounded-full p-2 cursor-pointer border border-white'>
+                  <FaInstagram className='text-2xl' />
+                </div>
+
+                <div className='w-[1px] h-[70px] bg-white'></div>
+              </div>
+            </div>
+          </div>
+        )}
+
 
       </section>
     </main>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from 'framer-motion';
 
-export const Navbar = () => {
-  return (
-    <nav className='absolute top-0 left-0 w-full  pt-10 text-white z-20'>
-        <div className='container'>
-           <div className='flex justify-between items-center'>
-            {/*logo section */}
-             
-               <h1 className='text-2xl font-semibold uppercase'>
-                 <span className='text-primary'>Coders</span> Coffee.
-                </h1>
+export const Navbar = ({openSidebar,setSidebarOpen}) => {
 
-            {/*menubar section */}
-             <div>
-                <GiHamburgerMenu className='text-3xl cursor-pointer'/>
-             </div>
-           </div>
-        </div>
-    </nav>
-  )
+    return (
+        <nav className='absolute top-0 left-0 w-full  pt-10 text-white z-40'>
+            <div className='container'>
+                <div className='flex justify-between items-center'>
+                    {/*logo section */}
+
+                    <motion.h1 className='text-2xl font-semibold uppercase'
+                        initial={{ opacity: 0, y: -100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 100,
+                            damping: 10,
+                            delay: 0.2,
+                        }}
+                    >
+                        <span className='text-primary'>Coders</span> Coffee.
+                    </motion.h1>
+
+                    {/*menubar section */}
+                    <motion.div
+                        initial={{ opacity: 0, y: -100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 100,
+                            damping: 10,
+                            delay: 0.2,
+                        }}
+
+                        onClick={()=> setSidebarOpen(!openSidebar)}
+                    >
+                        <GiHamburgerMenu className='text-3xl cursor-pointer' />
+                    </motion.div>
+                </div>
+            </div>
+        </nav>
+    )
 }
