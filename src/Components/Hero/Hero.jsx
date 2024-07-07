3269327d0641d75ef11ec326d4e39aa4f64b8e88
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-07-07 21:18:46 +0530

    adding framer motion animates to the hero section..

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import BgImage from "../../assets/bg-slate.png"
 import BlackCoffe from "../../assets/black.png"
+import { Navbar } from '../Navbar/Navbar'
+import {motion} from "framer-motion"
 
 const Hero = () => {
 
@@ -17,58 +19,114 @@ const Hero = () => {
         <div className='container'>
 
           {/*Navbar section */}
+            <Navbar/>
           {/*Hero section */}
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]'>
             {/*text content section */}
             <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
 
-              <h1 className='text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
-              <div className='relative'>
+              <motion.h1 className='text-7xl font-bold leading-tight ml-14'
+                initial={{opacity: 0, y: -100}}
+                animate={{opacity: 1, y: 0}}
+                 transition={{
+                  type:"spring",
+                   stiffness: 100,
+                   damping: 10,
+                   delay: 1,
+                   }}
+                >
+                Blvck Tumbler
+                </motion.h1>
+              <motion.div className='relative'
+                initial={{opacity: 0, y: 100}}
+                animate={{opacity: 1, y: 0}}
+                transition={{
+                 type:"spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 1.2,
+                  }}  
+               >
                 <div className='relative z-10 space-y-4'>
                   <h1 className='text-2xl'>Black Lifestyle Lovers</h1>
-                  <h1 className='text-sm opacity-55 leading-loose'>coffee is a celebration of culture, community, and creativity. Every sip fuels ambition and connection turning everyday moments into cherished rituals. Embrace the
-                    richness of coffee and elevate your lifestyle with every cup</h1>
+                  <h1 className='text-sm opacity-55 leading-loose'> Every sip fuels ambition and connection turning everyday moments into cherished rituals. Embrace the
+                    richness of coffee and elevate your lifestyle with every cup.</h1>
                 </div>
                 <div className='absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25 '>
                 </div>
-              </div>
+              </motion.div>
 
             </div>
 
             {/*Hero image section */}
             <div className='relative'>
-              <img src={BlackCoffe}
+              <motion.img src={BlackCoffe}
+                 initial={{opacity: 0, scale: 0}}
+                 animate={{opacity: 1, scale: 1}}
+                 transition={{
+                  type:"spring",
+                   stiffness: 100,   //it determines how fast the spring snaps back to its original position.Higher values results quicker animation and Lower values create a softer animation.
+                   damping: 10,      //it controls how much the spring oscillates or bounces before settling.Higher values results more damping, which slows down the animation.so lower values is better.
+                   delay: 0.4,
+                   }}  
                 alt='coffee-main'
                 className='relative z-40 h-[400px] md:h-[700px] img-shadow'
               />
               {/*orange Ring circle */}
-              <div className='absolute top-24 -right-16
-                      border-primary rounded-full w-[180px] h-[180px] border-[20px] z-10'>
-              </div>
+              <motion.div className='absolute top-24 -right-16
+                 border-primary rounded-full w-[180px] h-[180px] border-[20px] z-10'
+                 initial={{opacity: 0, y: 100}}
+                 animate={{opacity: 1, y: 0}}
+                 transition={{
+                  type:"spring",
+                   stiffness: 100,
+                   damping: 10,      
+                   delay: 0.8,
+                   }}  
+              >
+              </motion.div>
               {/*background big text */}
-              <div className='absolute -top-20 left-[200px] z-[1]'>
+              <motion.div className='absolute -top-20 left-[200px] z-[1]'
+               initial={{opacity: 0, x: -100}}
+               animate={{opacity: 1, x: 0}}
+               transition={{
+                type:"spring",
+                 stiffness: 100,
+                 damping: 10,
+                 delay: 0.8,
+                 }}  
+              >
                 <h1 className='text-[140px] scale-150 font-bold text-darkGray/40 leading-none '>
                   Blvck Tumbler
                 </h1>
-              </div>
+              </motion.div>
+
             </div>
 
             {/*Third div section */}
-            <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
+            <motion.div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'
+              initial={{opacity: 0, y: 100}}
+              animate={{opacity: 1, y: 0}}
+              transition={{
+               type:"spring",
+                stiffness: 100,
+                damping: 10,
+                delay: 1.2,
+                }}  
+            >
 
               <h1 className='opacity-0 text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
               <div className='relative'>
                 <div className='relative z-10 space-y-4'>
                   <h1 className='text-2xl'>Blvck Tumbler</h1>
                   <h1 className='text-sm opacity-55 leading-loose'>Experience the pure essence of coffee with our black tumbler brew. Bold, robust, and unapologetically rich—perfect 
-                    for those who savor the true taste of coffee.Simply savor the bold essence of black tumbler coffee.</h1>
+                    for those who savor the true taste of coffee.</h1>
                 </div>
-                <div className='absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50 '>
+                <div className='absolute -top-6 -right-10 w-[275px] h-[190px] bg-darkGray/50 '>
                 </div>
               </div>
 
-            </div>
-            <div></div>
+            </motion.div>
 
           </div>
         </div>
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
+
+export const Navbar = () => {
+  return (
+    <nav className='absolute top-0 left-0 w-full  pt-10 text-white z-20'>
+        <div className='container'>
+           <div className='flex justify-between items-center'>
+            {/*logo section */}
+             
+               <h1 className='text-2xl font-semibold uppercase'>
+                 <span className='text-primary'>Coders</span> Coffee.
+                </h1>
+
+            {/*menubar section */}
+             <div>
+                <GiHamburgerMenu className='text-3xl cursor-pointer'/>
+             </div>
+           </div>
+        </div>
+    </nav>
+  )
+}
