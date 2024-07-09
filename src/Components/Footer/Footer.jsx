9f3764b355a62b928c6a59f53eba1c6400d25930
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-07-09 16:52:55 +0530

    Footer section completes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import creditcards from "../../assets/creditcard.webp"
 import { FaFacebook,FaGoogle ,FaInstagram,FaPhone,FaTelegram} from "react-icons/fa";
 import { FaMapLocation } from "react-icons/fa6";
-import { motion } from 'framer-motion';
+
 
 
 const Footer = () => {
+
+    const year = new Date().getFullYear();
+
   return (
     <div className='bg-gradient-to-r from-primary to-primaryDArk pt-12 pb-8 text-white'>
         <div className='container'>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'>
-              {/**compant details */}
+              {/**company details */}
               <div className='space-y-6'>
                 <h1 className='text-3xl font-bold uppercase'>
                   Coders Cafe
@@ -29,9 +32,54 @@ const Footer = () => {
                 </div>
               </div>
               {/** Links section*/}
+              <div className='space-y-6'>
+                 <h1 className='text-3xl font-bold '>
+                    Quick Links
+                 </h1>
+                  <div className='grid grid-cols-2 gap-3'>
+                     <div>
+                        <ul className='space-y-2'>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact us</li>
+                            <li>Privacy Policy</li>
+                        </ul>
+                     </div>
+                     <div>
+                        <ul className='space-y-2'>
+                            <li>products</li>
+                            <li>service </li>
+                            <li>Details us</li>
+                            <li>Others</li>
+                        </ul>
+                     </div>
+                  </div>
+              </div>
               {/** social links*/}
-
+                <div className='space-y-6'>
+                       <h1 className='text-3xl font-bold'>
+                       Follow Us
+                       </h1>
+                       <div className='flex items-center gap-3'>
+                         <FaFacebook className='text-3xl hover:scale-105 duration-300'/>
+                         <FaInstagram className='text-3xl hover:scale-105 duration-300'/>
+                         <FaTelegram className='text-3xl hover:scale-105 duration-300'/>
+                         <FaGoogle className='text-3xl hover:scale-105 duration-300'/>
+                       </div>
+                       <div>
+                        <h1 className='text-xl mb-2 font-semibold'>Payment Methods</h1>
+                        <img src={creditcards} alt='FkU'
+                        className='w-[80%]'
+                        />
+                       </div>
+                </div>
             </div>
+
+             {/**copyright section */}
+
+             <p className='text-white text-center mt-8 pt-8 border-t-2'>
+                    Copyright &copy; {year} CodersCofffe. All rights reserved.
+                 </p>
         </div>
     </div>
   )
